e5d63bb854f2293967bf48ec6ea74367fc2f48c1
Grigoriy457 <grigori.p.vlasov@gmail.com>
2024-06-28 14:39:28 +0300

    Add steam auth for sale items

--- config.py
+++ config.py
@@ -6,7 +6,9 @@ config = dotenv_values(".env")
 ASF_HOSTS = config.get("ASF_HOSTS").split(",")
 ASF_PASSWORD = config.get("ASF_PASSWORD")
 
+STEAM_ID = config["STEAM_ID"]
 STEAM_SESSION_ID = config.get("STEAM_SESSION_ID")
+STEAM_LOGIN_SECURE = config["STEAM_LOGIN_SECURE"]
 
 FARM_AT_UTC_TIME = config.get("FARM_AT_UTC_TIME")
 
--- steam_api.py
+++ steam_api.py
@@ -18,10 +18,16 @@ class SteamItem:
 
 class SteamApi:
     def __init__(self):
+        self.app_id = APP_ID
+        self.context_id = CONTEXT_ID
+
+        self.steam_id = config.STEAM_ID
         self.session_id = config.STEAM_SESSION_ID
+        self.login_secure = config.STEAM_LOGIN_SECURE
+
         self.base_url = 'https://steamcommunity.com'
 
-    async def get_inventory(self, steam_id):
+    async def get_inventory(self):
         def is_card(tags):
             return dict([(tag["category"], tag["internal_name"]) for tag in tags]).get("item_class") == "item_class_2"
 
@@ -30,7 +36,7 @@ class SteamApi:
             "count": "2000"
         }
         async with aiohttp.ClientSession() as session:
-            async with session.get(f"{self.base_url}/inventory/{steam_id}/{APP_ID}/{CONTEXT_ID}", params=params) as response:
+            async with session.get(f"{self.base_url}/inventory/{self.steam_id}/{self.app_id}/{self.context_id}", params=params) as response:
                 data = await response.json()
                 descriptions = dict([(description["instanceid"], description) for description in data["descriptions"]])
                 items = []
@@ -43,23 +49,35 @@ class SteamApi:
 
                 return items
 
-    async def sell_item(self, asset_id, price):
+    async def sell_item(self, asset_id: str, price: int):
+        headers = {
+            "Accept": "*/*",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "ru-RU,ru;q=0.9,en-GB;q=0.8,en;q=0.7,en-US;q=0.6",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Cookie": f"sessionid={self.session_id}; steamLoginSecure={self.login_secure}",
+            "Referer": f"https://steamcommunity.com/profiles/{self.steam_id}/inventory"
+        }
         data = {
             "sessionid": self.session_id,
-            "appid": APP_ID,
-            "contextid": CONTEXT_ID,
+            "appid": self.app_id,
+            "contextid": self.context_id,
             "assetid": asset_id,
             "amount": 1,
             "price": price
         }
         async with aiohttp.ClientSession() as session:
-            async with session.post(f"{self.base_url}/market/sellitem", data=json.dumps(data)) as response:
+            async with session.post(f"{self.base_url}/market/sellitem", headers=headers, data=aiohttp.FormData(data)) as response:
                 return await response.json()
 
 
 async def test():
     steam_api = SteamApi()
-    pprint(await steam_api.get_inventory("76561199205197880"))
+    steam_api.app_id = "2923300"
+    steam_api.context_id = "2"
+    pprint(await steam_api.sell_item("4855525049031940705", 2))
 
 
 if __name__ == '__main__':
